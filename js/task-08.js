f97b1ad6d8ca4454fcc36910eb0926cea5fd7518
TrueMolni <truemolni@gmail.com>
2022-11-06 17:16:16 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -6,3 +6,27 @@
 // де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості.
 // Для доступу до елементів форми використовуй властивість elements.
 // 5.Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const formRef = document.querySelector(`.login-form`);
+
+console.log(formRef);
+
+const onSubmitHandle = (event) => {
+  event.preventDefault();
+  let userData = {};
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("всі поля повинні бути заповнені");
+  } else
+    userData = {
+      email: email.value,
+      password: password.value,
+    };
+  console.log(userData);
+  email.value = "";
+  password.value = "";
+};
+
+formRef.addEventListener("submit", onSubmitHandle);
